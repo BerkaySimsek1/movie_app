5b72074aebce545926d2454ec3b27445bbe241d8
BerkaySimsek1 <berkay.simsek.098@gmail.com>
2022-10-24 14:52:31 +0300

    creating pages

---
+++ lib/consts/api.dart
@@ -0,0 +1,7 @@
+const String apiKey = "";
+const String movieBaseUrl = "https://api.themoviedb.org/3";
+const String imageBaseUrl = "https://image.tmdb.org/t/p/w500/";
+const String defaultCastImage =
+    "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__480.png";
+const String defaultMovieImage =
+    "https://eticketsolutions.com/demo/themes/e-ticket/img/movie.jpg";
